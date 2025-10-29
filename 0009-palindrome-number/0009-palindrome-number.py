e4151d90ce16f36c83c2cd8be9413729f09d445e
developer-gokulk <110406805+developer-gokulk@users.noreply.github.com>
2025-10-29 20:45:15 +0530

    Time: 7 ms (62.53%), Space: 17.6 MB (89.07%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -3,15 +3,12 @@ class Solution:
         i=0
         x=str(x)
         j=len(x)-1
-        flag=False
         while i<=j:
-            if x[i] == x[j]:
-                i=i+1
-                j=j-1
-                flag=True
-            else:
-                flag=False
-                return flag
-        return flag    
+            if x[i] != x[j]:
+                return False
+            i=i+1
+            j=j-1
+        return True
+   
 
         
