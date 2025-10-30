5c1556942dd6bc8dc8e462f9882f7b4c588ef04e
developer-gokulk <110406805+developer-gokulk@users.noreply.github.com>
2025-10-30 22:58:17 +0530

    Time: 0 ms (100%), Space: 18.4 MB (73.16%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,10 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        p=0
+        for i in range(len(nums)):
+            if nums[i]==target:
+                return i
+            if nums[i] > target:
+                return i
+        return i+1 
+            
